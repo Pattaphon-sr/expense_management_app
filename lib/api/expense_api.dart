8d1a2d5f81b854afda99352a9baebd1a7a0c1690
thanawatjanhan <6631501046@lamduan.mfu.ac.th>
2025-08-26 02:08:54 +0700

    Add _showTodayExpense function and Update ExpenseApi class 2) Today expense

--- lib/api/expense_api.dart
+++ lib/api/expense_api.dart
@@ -5,7 +5,15 @@ class ExpenseApi {
   // 1) All expenses all(userId)  
 
   // 2) Today expense today(userId)
-  
+  Future<List<dynamic>> todayExpenses({required int userId}) async {
+  final res = await httpc.requestJson(
+    '/api/expenses/today',
+    method: HttpMethod.get,
+    query: {'userId': userId.toString()},
+  );
+  return res as List<dynamic>;
+}
+
   // 3) Search expense search(userId, keyword)
   Future<List<dynamic>> search({
     required int userId,
